0aadd48898da117cfd9c24cd91c93a6620cd3e28
Matthew Campbell <cattmampbell@gmail.com>
2019-01-21 16:27:54 -0500

    Organized Components in progrect.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
+import Stats from './Stats';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div id="app">
         <Navbar />
         <Header />
+        <Stats />
       </div>
     )
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import Avatar from './Avatar';
 import '../css/Header.css';
 
 const Header = () => {
   return (
     <React.Fragment>
       <main>
-        {/* Header Image */}
-        <div id="profileBackground" className="row" alt="Image courtesy of Elijah M. Henderson (@elijahhenderson) on Unsplash. More info here: https://unsplash.com/@elijahhenderson">
-          <div className="container">
-            
-          </div>
+        {/* User Header Background Image */}
+        <div id="userHeaderBackground" className="row" alt="Image courtesy of Elijah M. Henderson (@elijahhenderson) on Unsplash. More info here: https://unsplash.com/@elijahhenderson">
+          <Avatar />
         </div>
       </main>
     </React.Fragment>
